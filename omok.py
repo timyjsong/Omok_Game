3075387c739b72035508bd5784e002bdf0ea26c7
Tim Song <timyjsong@gmail.com>
2021-08-04 21:40:29 -0400

    Initial commit

---
+++ omok.py
@@ -0,0 +1,25 @@
+import tkinter as tk
+from board import Board
+
+"""
+requirements:
+
+win condition: 5 in a row, horizonal, vertical, diagonal
+turn counter
+checking if game won: after every turn
+board size: 19 x 19
+3 classes: 1 for drawing, 1 for points (coords), 1 for pieces
+"""
+
+
+def main():
+    root = tk.Tk()
+    board = Board(root, 840) # 14 x 60 = 840
+    root.mainloop()
+
+
+if __name__ == "__main__":
+    main()
+
+
+
---
+++ square.py
@@ -0,0 +1,41 @@
+
+
+class Square(object):
+
+    side_len = 60
+    color = "#A9602B"
+
+    def __init__(self, canvas, row, col):
+        self.canvas = canvas
+        self.row, self.col = row, col
+
+    def __str__(self):
+        return f"row: {self.row}, col: {self.col}"
+
+    @property
+    def x_1(self):
+        return self.side_len * self.col
+
+    @property
+    def y_1(self):
+        return self.side_len * self.row
+
+    @property
+    def x_2(self):
+        return self.x_1 + self.side_len
+
+    @property
+    def y_2(self):
+        return self.y_1 + self.side_len
+
+    @property
+    def coords(self):
+        return self.x_1, self.y_1, self.x_2, self.y_2
+
+    def draw(self):
+        self.canvas.create_rectangle(*self.coords, fill=self.color)
+
+
+
+
+
